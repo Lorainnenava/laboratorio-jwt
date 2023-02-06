63fd421773fe63aa1c7f9dfbfa0d5163683e2cd5
Lorainnenava <lorainnenavarro0723@gmail.com>
2023-02-06 10:56:30 -0500

    listo

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
+const express = require('express');
 const app = express();
+const jsonwebtoken = require('jsonwebtoken');
+require('dotenv').config(users);
 
 const users = [
   { email: "admin@example.com", name: "admin", rol: "admin" },
@@ -10,5 +13,11 @@ app.use(express.json());
 app.get("/", function (req, res) {
   res.send("Bienvenido a la api de ADA Cars");
 });
+
+app.post('/auth', (req, res) => {
+  const email= req.body.email;
+  res.json({email: email})
+  res.end()
+})
 
 module.exports = app;
